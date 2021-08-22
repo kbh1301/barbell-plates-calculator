c4fc387ee633038c52e93e93ce90e52f1457029c
kbh1301 <kyle.hulvey@gmail.com>
2021-08-21 19:25:49 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Footer from './components/Footer/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Navigation /> */}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ContactBar from '../ContactForm/ContactBar';
+
+const Footer = () => {
+    return(
+        <footer className="flex justify-end items-center bg-dark-gray pv2 ph4-m ph5-l white">
+            <small className="f6 db tr">© 2021 Developed by <b class="ttu light-blue">Kyle Hulvey</b></small>
+            <div className="ml4"><ContactBar /></div>
+        </footer>
+    );
+}
+
+export default Footer;
