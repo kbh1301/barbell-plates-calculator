064000a3b6ccd8efa68b1f12aa5f9b5f8aa878fb
kbh1301 <kyle.hulvey@gmail.com>
2021-09-16 19:46:13 -0500

    - CSS Changes - Moved bulk of calculations to util.js - Input component now only sends input information to app.js - App.js now redirects input information to util.js and distributes results - Added WeightInfo component and corresponding util calculations

--- src/components/WeightInfo.js
+++ src/components/WeightInfo.js
@@ -1,7 +1,18 @@
-const WeightInfo = () => {
+import './WeightInfo.css';
+
+const WeightInfo = ({ countObj }) => {
+    // sort countObj; for each element, add info component
+    const buildPlateInfo = () => Object.keys(countObj)
+        .sort((a,b) => b - a)
+        .map((i) => {
+            return <div>{countObj[i]} x {i} lb</div>
+        });
+
+    // complete WeightInfo
     return(
-        <div>
-            <p>test</p>
+        <div className="weightInfoNumber">
+            PER SIDE<br/>
+            {buildPlateInfo()}
         </div>
     );
 }
