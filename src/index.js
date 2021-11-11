a79de50b7c8c21e5dbc2ee4c86fc3ee2b515e8ab
kbh1301 <kyle.hulvey@gmail.com>
2021-11-10 19:29:00 -0600

    - renames - CSS changes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import BarbellPlatesCalculator from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BarbellPlatesCalculator />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('barbellPlatesCalcRoot')
 );
 
 // If you want to start measuring performance in your app, pass a function
