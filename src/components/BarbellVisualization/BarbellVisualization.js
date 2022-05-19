8a5e90567a8b5fd7c4dd5d000dd73b0ecc820dda
kbh1301 <kyle.hulvey@gmail.com>
2022-05-18 22:50:41 -0500

    Repo update

--- src/components/BarbellVisualization/BarbellVisualization.js
+++ src/components/BarbellVisualization/BarbellVisualization.js
@@ -1,7 +1,6 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useRef, useState } from 'react';
 import '../WeightInput/WeightInput.scss'
 import barbell_shaftImg from '../img/barbell_shaft.png'
-import barbell_sleeveCapImg from '../img/barbell_sleeveCap.png'
 import barbell_sleeveImg from '../img/barbell_sleeve.png'
 import barbell_sleeveCutImg from '../img/barbell_sleeveCut.png'
 import plateImg from '../img/plate.png'
@@ -10,8 +9,10 @@ const BarbellVisualization = ({ inputArray }) => {
     // for each plate in inputArray, create img component
     const buildPlateImgs = () => inputArray.map((plate, i) => {
         return(
-            <div key={i} className="flex-cell">
-                <img className="plate-img" src={plateImg} alt="" style={{height:`${plate.plateSize}%`}}/>
+            <div className="flex-cell">
+                <img key={i} className="plate-img" src={plateImg} alt=""
+                    style={{height:`${plate.plateSize}%`}}
+                />
             </div>
         );
     });
@@ -19,20 +20,19 @@ const BarbellVisualization = ({ inputArray }) => {
     // complete BarbellVisualization component
     return (
         <div className="barbell-visualization-container">
-            <div className="barbell-visualization">
-                <div className="shell">
-                    <img className="barbell-img" src={barbell_shaftImg} alt="" />
-                    <img className="barbell-img" src={barbell_sleeveCapImg} alt="" />
-                    { // if inputArray is empty, return empty sleeve; else, return plateStack weights and shortened sleeve
-                    !inputArray.length
-                        ? <img className="barbell-img" src={barbell_sleeveImg} alt="" />
-                        :   <Fragment>
-                                {buildPlateImgs()}
-                                <img className="barbell-img" src={barbell_sleeveCutImg} alt="" />
-                            </Fragment>
-                    }
-                </div>
+            <div className="flex-cell">
+                <img className="barbell-img" src={barbell_shaftImg} alt="" />
             </div>
+            { // if inputArray is empty, return empty sleeve; else, return plateStack weights and shortened sleeve
+            !inputArray.length
+                ? <img className="barbell-img" src={barbell_sleeveImg} alt="" />
+                :   <Fragment>
+                        {buildPlateImgs()}
+                        <div className="flex-cell">
+                            <img className="barbell-img" src={barbell_sleeveCutImg} alt="" />
+                        </div>
+                    </Fragment>
+            }
         </div>
     );
 }
